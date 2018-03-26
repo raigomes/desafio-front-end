49ea18f08597965fd7c50eef37cc8cbc3c9f347a
raigomes <rai.gomes.ismart@gmail.com>
2018-03-26 01:22:01 -0300

    Implementando o layout de dois dos três tipos de destaques de matérias

--- htdocs/assets/script/main.js
+++ htdocs/assets/script/main.js
@@ -1,17 +1,36 @@
 function insertElementIntoHTML (content) {
-    var section = document.getElementById(content.name);
-    var newSection = "<section>";
+    var name = content.name;
+    var section = document.getElementById(name);
+    var newSection = `<section class="row">`;    
+    var template = "";
 
     content.data.forEach(data => {
-        newSection += `<section>
-                            <img src="/assets/media/${data.image}" alt="${data.image}" />
-                            <h3>${data.label}</h3>
-                            <strong>${data.title}</strong>
-                            <p>${data.description}</p>
-                        </section>`;
-    });
+        if(data.image.includes("464x261")) {
+            template = "destaque-principal";
+        } else {
+            if(data.image.includes("216x216")) {
+                template = "destaque-dois";
+            }
+            else {
+                template = "destaque-tres";
+            }
+        }
 
-    newSection += "</section>";
+        newSection +=   `<section class="${name} ${template} container">
+                            <section class="news-image">
+                                <img src="/assets/media/${data.image}" alt="${data.image}" />                                
+                                <section class="share"></section>
+                            </section>                            
+                            <section class="news">
+                                <h3 class="news-section">${data.label.toUpperCase()}</h3>
+                                <h4 class="news-title">${data.title}</h4>
+                                <p class="news-description">${data.description}</p>
+                            </section>
+                            
+                        </section>`;        
+    });
+    
+    newSection += `</section>`;
 
     section.innerHTML += newSection;
 }
